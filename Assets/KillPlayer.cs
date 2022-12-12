d643b24f0f93fcc0c45cec55b7e4c2a5e6a84303
jojo2723 <ccis.joshua.lim@gmail.com>
2022-12-12 12:22:28 +0800

    RDzone Update
    Respawn and Death Prefabs

---
+++ Assets/KillPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    public Transform respawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.position;
+        }
+    }
+}
